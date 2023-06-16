2b25404f734b7decf05c996f8614f9e8742ae0a8
Sara Bobo <sara.bobo@datadoghq.com>
2023-06-16 14:57:35 -0400

    add install script

---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Printing our text in purple (technically blue but w/e)
+function print {
+  echo -e "\033[34m$1\033[0m"
+}
+
+function link {
+  file="$1"
+  if [ ! -e ~/$file -a ! -L ~/$file ]; then
+    print "Creating link for $file"
+    ln -s ~/dotfiles/$file ~/$file
+  else
+    print "$file exists, skipping"
+  fi
+}
+
+# Symlinks
+link .vimrc
+link .tmux.conf
+print "Created symlinks"
+
+# Installing vim Plug and plugins
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim -es -u vimrc -i NONE -c "PlugInstall" -c "qa"
+print "vim plugins installed"
+
+# For MacOS, grab stuff for iTerm
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # Installing Powerline fonts
+  git clone https://github.com/powerline/fonts.git --depth=1
+  cd fonts
+  ./install.sh
+  cd ..
+  rm -rf fonts
+  print "Set up Powerline fonts"
+  if [ ! -e ~/Monokai%20Soda.itermcolors ]; then
+    curl -fLo ~/Monokai%20Soda.itermcolors https://raw.githubusercontent.com/mbadolato/iTerm2-Color-Schemes/master/schemes/Monokai%20Soda.itermcolors
+    print "Monokai Soda iterm template downloaded to home dir"
+  fi
+fi
+print "All set up! Have fun storming the castle!"
